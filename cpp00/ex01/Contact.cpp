94458db13f5ce61bd177daaf77dd06e0df84b091
ghwa <gaebelhdz@gmail.com>
2024-03-27 09:45:02 +0800

    thanks rsoo

---
+++ cpp00/ex01/Contact.cpp
@@ -0,0 +1,20 @@
+
+#include "Contact.hpp"
+
+Contact::Contact() {
+
+}
+
+Contact::~Contact() {
+
+}
+
+std::string Contact::getFirstName() const {
+
+}
+// ...
+
+void Contact::setFirstName( std::string newFirstName ) {
+	this->first_name = newFirstName;
+}
+// ...
--- cpp00/ex01/Contact.hpp
+++ cpp00/ex01/Contact.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/05 13:51:03 by ghwa              #+#    #+#             */
-/*   Updated: 2024/03/27 09:36:30 by ghwa             ###   ########.fr       */
+/*   Created: 2024/03/27 09:36:44 by ghwa              #+#    #+#             */
+/*   Updated: 2024/03/27 09:37:01 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 #include <string>
 #include <iomanip>
 
-class PhoneBook {
+class Contact {
 	public:
-		PhoneBook() {
-			;
-		}
-		void printheader() {
-			for (int i = 0; i < 56;i++)
-				std::cout << "_";
-			std::cout << std::endl;
-			std::cout << "|" << std::setw(10) << "First Name" << "|";
-			std::cout << std::setw(10) << "Last Name" << "|";
-			std::cout << std::setw(10) << "Nickname" << "|";
-			std::cout << std::setw(10) << "Phone No." << "|";
-			std::cout << std::setw(10) << "Secret" << "|" << std::endl;
-		}
+		Contact();
+		~Contact();
+		
+		std::string getFirstName() const;
+		// ...
+
+		void setFirstName( std::string newFirstName );
+		// ...
 	private:
+		std::string first_name;
+		std::string last_name;
+		std::string nickname;
+		std::string phone_number;
+		std::string darkest_secret;
 } ;
