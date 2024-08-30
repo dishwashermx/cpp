dedb747772b84d637b7b5ac929e43147369e5ed4
Gaebel Hwa <ghwa@u90z04s02.42kl.edu.my>
2024-08-30 16:36:59 +0800

    friday

---
+++ cpp03/ex00/ClapTrap copy.cpp
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 13:49:32 by ghwa              #+#    #+#             */
+/*   Updated: 2024/08/30 16:24:04 by ghwa             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap() : name("[Cl4P-TP]"){
+	std::cout << "Clapbot activated." << std::endl;
+}
+
+ClapTrap::ClapTrap(std::string name) : name(name) {
+	std::cout << name << " activated." << std::endl;
+	_hp = 10;
+	_energy = 10;
+	_dmg = 0;
+}
+
+ClapTrap::ClapTrap(const ClapTrap &other) {
+	std::cout << this.name << " cloned." << std::endl;
+	*this = other;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &other) {
+	std::cout << other.name << " morphed into " this.name << std::endl;
+	this->_hp = other._hp;
+	this->_energy = other._energy;
+	this->_dmg = other._dmg;
+	return (*this);
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << name << " terminated." << std::endl;
+}
+
+void ClapTrap::attack(const std::string& target) {
+	std::cout << name << "attacks " << _target << \
+	", causing " << _dmg << " points of damage." << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	std::cout << name << "takes " << amount << " of damage." << std::endl;
+	_hp -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+	std::cout << name << " repairs for " << amount << " of hp." << std::endl;
+}
+}
---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/30 13:49:58 by ghwa              #+#    #+#             */
+/*   Updated: 2024/08/30 16:24:09 by ghwa             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+#include <iostream>
+
+class ClapTrap {
+	private:
+		std::string name;
+		int _hp;
+		int _energy;
+		int _dmg;
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &other);
+		ClapTrap& operator=(const ClapTrap& other);
+		~ClapTrap();
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+} ;
+
+#endif
