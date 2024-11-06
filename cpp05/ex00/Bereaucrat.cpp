5a531f68ebd579228b3e2c9e5fa2eb0281712533
Gaebel Hwa <ghwa@u81z05s02.42kl.edu.my>
2024-11-06 09:31:17 +0800

    pls

--- cpp05/ex00/Bereaucrat.cpp
+++ cpp05/ex00/Bereaucrat.cpp
@@ -6,25 +6,26 @@
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 17:12:07 by ghwa              #+#    #+#             */
-/*   Updated: 2024/11/05 11:28:55 by ghwa             ###   ########.fr       */
+/*   Updated: 2024/11/06 09:23:44 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat() : _name("Bureaucriti"), _grade(150) {
+Bureaucrat::Bureaucrat() : _name("Lackey"), _grade(150) {
 	std::cout << "Bureaucrat Default Constructor called" << std::endl;
 }
 
-Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name), _grade(grade) {
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name) {
 	std::cout << "Bureaucrat Constructor called" << std::endl;
+	setGrade(grade);
 }
 
-Bureaucrat::Bureaucrat(const Bureaucrat &other) {
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), _grade(other._grade) {
 	std::cout << "Bureaucrat Copy Constructor called" << std::endl;
 }
 
-Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) : _name(other._name), _grade(other._grade) {
 	std::cout << "Bureaucrat Copy Assignment called" << std::endl;
 	if (this != &other)
 		return (*this);
@@ -34,10 +35,40 @@ Bureaucrat::~Bureaucrat() {
 	std::cout << "Bureaucrat Destructor called" << std::endl;
 }
 
+const std::string Bureaucrat::getName() const {
+	return (this->_name);
+}
+
+const int Bureaucrat::getGrade() const {
+	return (this->_grade);
+}
+
+void Bureaucrat::incrementGrade() {
+	setGrade(this->getGrade() - 1);
+}
+
+void Bureaucrat::decrementGrade() {
+	setGrade(this->getGrade() + 1);
+}
+
+void Bureaucrat::setGrade(int newGrade) {
+	if (newGrade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else if (newGrade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	else
+		this->_grade = newGrade;
+}
+
 const char *Bureaucrat::GradeTooLowException::what(void) const throw() {
 	return ("Grade too low");
-};
+}
 
 const char *Bureaucrat::GradeTooHighException::what(void) const throw() {
 	return ("Grade too high");
-};
+}
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat) {
+	out << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return (out);
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: ghwa <ghwa@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 17:12:05 by ghwa              #+#    #+#             */
-/*   Updated: 2024/11/05 11:29:52 by ghwa             ###   ########.fr       */
+/*   Updated: 2024/11/06 09:22:09 by ghwa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 class Bureaucrat {
 	private:
-		setGrade(int newGrade);
+		void setGrade(int newGrade);
 		const std::string _name;
 		int _grade;
 	public:
@@ -27,6 +27,12 @@ class Bureaucrat {
 		Bureaucrat(const Bureaucrat &other);
 		Bureaucrat& operator=(const Bureaucrat& other);
 		~Bureaucrat();
+
+		const std::string getName() const;
+		const int getGrade() const;
+		void incrementGrade();
+		void decrementGrade();
+
 		class GradeTooLowException : public std::exception {
 			public:
 				virtual const char *what() const throw();};
@@ -34,5 +40,7 @@ class Bureaucrat {
 			public:
 				virtual const char *what() const throw();};
 } ;
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& bureaucrat);
 
 #endif
